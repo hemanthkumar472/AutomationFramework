5ce3834fb3bf020107fb2d0a20718994f8703458
rawat <rawat@Radhe_Radhe>
2026-01-22 12:50:33 +0530

    First Commit From Vishal.

---
+++ mavenpractiseproject/src/test/java/samplepackage/Sample1FORPULLTest.java
@@ -0,0 +1,14 @@
+package samplepackage;
+
+import org.testng.annotations.Test;
+
+public class Sample1FORPULLTest {
+	
+	@Test
+	public void sample() {
+
+		System.out.println("*******>>>>>>>>>>>>>>>>>>>>>>>>MAVEN TESTING DONE<<<<<<<<<<<<<<<<<<<<*********");
+		
+	}
+
+}
