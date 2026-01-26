41580b533748ceb9338fdbfdb270c12aeee77d74
rawat <rawat@Radhe_Radhe>
2026-01-26 12:28:24 +0530

    commit for jenkins demo

---
+++ mavenpractiseproject/src/test/java/samplepackage/SampleJenkinsTest.java
@@ -0,0 +1,14 @@
+package samplepackage;
+
+import org.testng.annotations.Test;
+
+public class SampleJenkinsTest {
+	
+	@Test
+	public void sample() {
+
+		System.out.println("*******>>>>>>>>>>>>>>>>>>>>>>>>Jenkins TESTING DONE<<<<<<<<<<<<<<<<<<<<*********");
+		
+	}
+
+}
